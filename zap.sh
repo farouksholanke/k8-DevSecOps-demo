3ee3c4229976817454bf8d66f5da0272123435b6
Farouk Sholanke <zayd.farouk@gmail.com>
2025-01-21 19:20:21 -0500

    added owasp zap

--- zap.sh
+++ zap.sh
@@ -5,30 +5,27 @@ PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].node
 # first run this
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+# docker run -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
 
 # comment above cmd and uncomment below lines to run with CUSTOM RULES
-docker pull owasp/zap2docker-weekly
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
+docker pull zaproxy/zap-weekly
+docker run -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
 
 exit_code=$?
 
-
 # HTML Report
- sudo mkdir -p owasp-zap-report
- sudo mv zap_report.html owasp-zap-report
-
+sudo mkdir -p owasp-zap-report
+sudo mv zap_report.html owasp-zap-report
 
 echo "Exit Code : $exit_code"
 
- if [[ ${exit_code} -ne 0 ]];  then
+if [[ ${exit_code} -ne 0 ]]; then
     echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
     exit 1;
-   else
+else
     echo "OWASP ZAP did not report any Risk"
- fi;
-
+fi;
 
 # Generate ConfigFile
-# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t http://devsecops-demo.eastus.cloudapp.azure.com:31933/v3/api-docs -f openapi -g gen_file
+# docker run -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-weekly zap-api-scan.py -t http://devsecops-demo.eastus.cloudapp.azure.com:31933/v3/api-docs -f openapi -g gen_file
